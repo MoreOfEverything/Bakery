826169350fb30d6acac44bbef5d11afd4b491b41
Henry Moore <henryrmoore@btinternet.com>
2016-11-07 15:30:30 +0000

    Spaced out recipe

--- src/main/java/org/moreofeverything/bakery/item/crafting/RecipesFood.java
+++ src/main/java/org/moreofeverything/bakery/item/crafting/RecipesFood.java
@@ -14,7 +14,11 @@ public class RecipesFood
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.EGG_BOWL), new ItemStack(Items.BOWL), new ItemStack((Items.EGG)));
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.DOUGH), new ItemStack(ModItems.EGG_BOWL.setContainerItem(Items.BOWL)), new ItemStack(Items.POTIONITEM.setContainerItem(Items.GLASS_BOTTLE)), new ItemStack(ModItems.FLOUR));
         GameRegistry.addShapelessRecipe(new ItemStack(ModItems.FLOUR), new ItemStack(ModItems.MORTAR_AND_PESTLE), new ItemStack(Items.WHEAT));
-        GameRegistry.addShapedRecipe(new ItemStack(ModItems.MORTAR_AND_PESTLE), "  A", "BCB", " B ", 'A', new ItemStack(Items.STICK), 'B', new ItemStack(Blocks.STONE,1,1), 'C', new ItemStack(Blocks.STONE,1,2));
+        GameRegistry.addShapedRecipe(new ItemStack(ModItems.MORTAR_AND_PESTLE),
+                "  A",
+                "BCB",
+                " B ",
+                'A', new ItemStack(Items.STICK), 'B', new ItemStack(Blocks.STONE,1,1), 'C', new ItemStack(Blocks.STONE,1,2));
     }
 
     public static void removeRecipes()
