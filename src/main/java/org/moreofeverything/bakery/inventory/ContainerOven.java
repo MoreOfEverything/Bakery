014462a477b4e5025eac134008a791b347442fce
Henry Moore <henryrmoore@btinternet.com>
2016-11-26 02:18:44 +0000

    stuff

--- src/main/java/org/moreofeverything/bakery/inventory/ContainerOven.java
+++ src/main/java/org/moreofeverything/bakery/inventory/ContainerOven.java
@@ -2,7 +2,6 @@ package org.moreofeverything.bakery.inventory;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.inventory.*;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
